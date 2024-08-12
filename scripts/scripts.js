34f27b405c33ff3f904ea457dbffb4b9269087a1
Leonardo Zamarchi <leonardozamarchi@gmail.com>
2024-08-11 22:57:27 -0300

    english

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function setLanguage(language) {
+    document.querySelectorAll('[data-en]').forEach(element => {
+        element.innerText = element.getAttribute(`data-${language}`);
+    });
+    localStorage.setItem('language', language);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const language = localStorage.getItem('language') || 'en';
+    setLanguage(language);
+});
